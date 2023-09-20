69ec7cf3c35fd1781a08525ee8e47c1f0f18b70e
Intelligence247 <ua67527056@gmail.com>
2023-09-20 08:50:14 +0100

    day 7 completed

--- REDUX-DEMO/index.js
+++ REDUX-DEMO/index.js
@@ -3,14 +3,20 @@ const redux = require('redux')
 const createStore = redux.createStore
 // Actions
 const CAKE_ORDERED = 'CAKE_ORDERED';
+const RESTOCK_CAKE = 'RESTOCK_CAKE';
 
-function OrderCake(){ 
+function OrderCake(qty=1){ 
     return {
     type: CAKE_ORDERED,
-    quantity: 1,
+    payload: qty,
 }
 }
- 
+ function RestockCake(qty=1){
+    return{
+        type: RESTOCK_CAKE,
+        payload: qty
+    }
+ }
 const initialState = {
     numOfCake: 10,
 }
@@ -23,8 +29,13 @@ const reducer = (state = initialState, action) => {
         case CAKE_ORDERED:
         return{
             ...state,
-            numOfCake: state.numOfCake - 1,
+            numOfCake: state.numOfCake - action.payload,
         };
+        case RESTOCK_CAKE:
+            return{
+                ...state,
+                numOfCake: state.numOfCake + action.payload
+            }
         default: {
             return state
         }
@@ -39,9 +50,11 @@ const reducer = (state = initialState, action) => {
  console.log('update state', store.getState())
  )
 
- store.dispatch(OrderCake())
- store.dispatch(OrderCake())
- store.dispatch(OrderCake())
- store.dispatch(OrderCake())
-
+ store.dispatch(OrderCake(2))
+store.dispatch(RestockCake(3))
  unsubscribe()
+
+
+//  SSH-KEY Process 
+// 1. ssh-keygen -o
+// 2.  
