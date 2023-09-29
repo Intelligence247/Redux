797da1d67e09af1b0e2e3b0e112ce0a908ccbec2
Intelligence247 <ua67527056@gmail.com>
2023-09-29 15:39:29 +0100

    Replaced console with redux-logger

--- RTK-DEMO/App/store.js
+++ RTK-DEMO/App/store.js
@@ -1,13 +1,19 @@
 const configureStore = require('@reduxjs/toolkit').configureStore
+const reduxLogger = require('redux-logger')
 const cakeReducer = require('../features/cake/CakeSlice')
 const iceCreamReducer = require('../features/icecream/icecreamSlice')
 const chickenlabReducer = require('../features/chickenLap/oneChickenLap')
+const { getDefaultMiddleware } = require('@reduxjs/toolkit')
+
+const logger = reduxLogger.createLogger()
+
 const store = configureStore({
     reducer: {
         cake: cakeReducer,
         icecream: iceCreamReducer,
         chickenlab: chickenlabReducer,
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 })
 
 module.exports = store
